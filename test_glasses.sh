1f60f97e031229b77874bd8b2c13abde0b69b471
Andy Li <andy@onthewings.net>
2017-10-30 10:07:38 +0800

    added glasses scripts

---
+++ test_glasses.sh
@@ -0,0 +1,1 @@
+python test.py --dataroot ./datasets/glasses --name glasses_cyclegan --model cycle_gan --no_dropout --nThreads 1 --loadSize 200 --fineSize 200 --ngf 32 --ndf 32
---
+++ train_glasses.sh
@@ -0,0 +1,1 @@
+python train.py --dataroot ./datasets/glasses --name glasses_cyclegan --model cycle_gan --pool_size 50 --no_dropout --nThreads 1 --loadSize 200 --fineSize 200 --ngf 32 --ndf 32 --continue_train
